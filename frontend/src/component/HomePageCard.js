f4184350f1b59b2bdf406fd8dc9cb847a22455ea
subhadippal66 <subhadippal66@gmail.com>
2021-03-13 17:41:46 +0530

    frosted glass effect in Homepage

--- frontend/src/component/HomePageCard.js
+++ frontend/src/component/HomePageCard.js
@@ -8,10 +8,24 @@ export default function OutlinedCard({
 }) {
   return (
     <div className="my-4 flex flex-col items-center" style={{ width: "95vw" }}>
-      <div className=" font-extrabold text-2xl rounded-2xl transform translate-y-4 py-2 px-12 text-center min-w-max z-10 bg-yellow-400">
+      <div
+        className=" lowercase font-extrabold text-2xl rounded-2xl transform translate-y-4 py-2 px-12 text-center min-w-max z-10"
+        style={{
+          backdropFilter: "blur(5px)",
+          background:
+            "linear-gradient(90deg, rgba(255,235,0,0.5) 0%, rgba(253,187,45,0.5) 100%)",
+        }}
+      >
         <h1>{heading}</h1>
       </div>
-      <div className="flex flex-row justify-between rounded-2xl w-5/6 bg-white">
+      <div
+        className="flex flex-row justify-between rounded-2xl w-5/6 bg-white"
+        // style={{
+        //   background:
+        //     "linear-gradient(90deg, rgba(34,193,195,0.5) 0%, rgba(253,187,45,0.5) 100%)",
+        //   backdropFilter: "blur(20px)",
+        // }}
+      >
         <div className="flex flex-col justify-start items-start ml-5 mt-6">
           <div className="font-mono font-bold text-xl mb-5">{details}</div>
           <div className="inline-block mb-4">
@@ -28,7 +42,14 @@ export default function OutlinedCard({
           </div>
         </div>
 
-        <div className="py-6 flex flex-col justify-center items-center bg-yellow-400 rounded-r-2xl  cursor-pointer">
+        <div
+          className="py-6 flex flex-col justify-center items-center  rounded-r-2xl  cursor-pointer bg-yellow-300"
+          // style={{
+          //   backdropFilter: "blur(20px)",
+          //   background:
+          //     " linear-gradient(90deg, rgba(252,211,0,0.5) 0%, rgba(75,84,0,0.5) 100%)",
+          // }}
+        >
           <NavigateNextIcon fontSize="large" />
         </div>
       </div>
